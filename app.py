5331cd55764199b51f07beca9b1f19862060e413
peruql <112070393+peruql@users.noreply.github.com>
2024-07-26 19:16:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def initialize_firebase():
         "type": os.environ.get("FIREBASE_TYPE"),
         "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
         "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
         "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
         "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
         "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
